5dd8065b4a41cfaf998d16d7a8947c919f87bc2f
Oleksandr <sasha291108@gmail.com>
2026-03-27 20:19:38 +0100

    Final SEO setup: robots, sitemap and meta tags

--- social_media/urls.py
+++ social_media/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import api_urls
+from django.views.generic.base import TemplateView # Додай цей імпорт зверху
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,6 @@ urlpatterns = [
 	path('chat/', include('chat.urls')),
 	path('notifications/', include('notifications.urls')),
     path('api/', include(api_urls)),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
 ]
